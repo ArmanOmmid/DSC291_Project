0c1e8dbc176026f3f2bb92db8bf88baca109ef01
zsadler <alsozatch@gmail.com>
2023-06-15 18:17:32 +0000

    implemented fisher loss

--- src/arch/dsc/dsc_arch.py
+++ src/arch/dsc/dsc_arch.py
@@ -3,11 +3,12 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-#ToDO Fill in the __ values
 class Smoother(nn.Module):
     def __init__(self, num_classes, config):
         super().__init__()
 
+        self.device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
         self.base_criterion = nn.CrossEntropyLoss()
 
         self.latent_smoothing = config.latent_smoothing
@@ -72,13 +73,16 @@ class Smoother(nn.Module):
         return result
 
     def forward(self, input):
-
         if not self.latent_smoothing:
             return self.standard_forward(input)
         
         mu, log_var = self.encode(input)
         self.mu, self.log_var = mu, log_var
+        
+        # z is shape [batch_size, latent_dim]
         z = self.reparameterize(mu, log_var)
+        self.z = z
+      
         output = self.decode(z)
 
         return  output
@@ -86,7 +90,48 @@ class Smoother(nn.Module):
     def register_base_criterion(self, criterion):
         self.base_criterion = criterion
 
+    def get_class_averages(self, z, labels, num_classes):
+        class_averages = torch.zeros(num_classes, z.size(1), device=self.device)  # Initialize tensor to store class averages
+        class_counts = torch.zeros(num_classes, device=self.device)
+
+        for i in range(num_classes):
+            class_indices = torch.nonzero(labels == i).squeeze() # Get indices where the label for class i is 1
+            if class_indices.numel() == 1:
+                class_indices = class_indices.unsqueeze(0)
+            class_z = z[class_indices]  # Get the corresponding z values for class i
+            class_average = torch.mean(class_z, dim=0)  # Compute the average over z for class i
+            class_averages[i] = class_average
+            class_counts[i] = len(class_indices)
+    
+        return class_averages, class_counts
+        
     def loss_function(self, output, labels):
+        # outputs shape [batch_size, num_classes]
+        # labels shape [batch_size]
+        
+        # --- Fisher Loss ---
+        num_classes = output.shape[1]
+        z_bar = torch.mean(self.z) # shape [latent_dim]
+        z_bar_i, class_counts = self.get_class_averages(self.z, labels, num_classes) # shape [num_classes, latent_dim]
+        
+        diffs = (z_bar_i - z_bar.unsqueeze(0)) # unsqueeze ensures correct broadcasting dimension
+        expanded_diffs = diffs.unsqueeze(-1) # [num_classes, latent_dim, 1]
+        
+        S_b = torch.sum((expanded_diffs @ expanded_diffs.transpose(1,2)) * class_counts.unsqueeze(-1).unsqueeze(-1),dim=0)
+        
+        
+        S_w = torch.zeros(self.latent_dim, self.latent_dim, device=self.device)
+        for z_sample, lbl in zip(self.z, labels):
+            diff = z_sample - z_bar_i[lbl]
+            expanded_diff = diff.unsqueeze(-1)
+            prod = expanded_diff @ expanded_diff.transpose(0,1)
+            S_w += prod
+            
+        print(S_w)
+        
+        fisher_loss_1 = torch.norm(S_w)
+        fisher_loss_2 = torch.norm(S_b)
+        
         if not self.latent_smoothing:
             return self.base_criterion(output, labels)
 
@@ -94,6 +139,6 @@ class Smoother(nn.Module):
 
         kld_loss = torch.mean(-0.5 * torch.sum(1 + self.log_var - self.mu ** 2 - self.log_var.exp(), dim = 1), dim = 0) # Analytic KL Divergence Loss from isotropic gaussian
 
-        loss = label_loss + self.kl_weight * kld_loss
+        loss = label_loss + self.kl_weight * kld_loss + 0.02*fisher_loss_1 - 0.02*fisher_loss_2
 
         return loss
